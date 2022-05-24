719f90dcf7ec3fc3aa9dd1683e5a67cbe74486fa
Maksym Bryvus <dazzle132453@gmail.com>
2022-05-24 19:36:05 +0300

    1

--- server/src/main/java/com/nukem/nothingreloaded/config/WebSecurityConfig.java
+++ server/src/main/java/com/nukem/nothingreloaded/config/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                  .and()
                      .authorizeRequests()
-                     .antMatchers("/posts", "/login", "/registration").permitAll()
+                     .antMatchers("/posts/**", "/login", "/registration").permitAll()
                      .anyRequest().authenticated()
                 .and()
                     .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
